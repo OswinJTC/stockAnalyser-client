de48bc900e0daf2c1f32f75f1bef6d9dd60c4f1d
Oswin <129711775+OswinJTC@users.noreply.github.com>
2024-03-27 17:59:09 +0800

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import config from "./config";
 
 export default axios.create({
-    baseURL: config.apiUrl,
+    baseURL: "http://18.136.213.130/",  
     headers: {"skip-browser-warning": "true"}
-}); 
+});
